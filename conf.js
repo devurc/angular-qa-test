ae1103af672a39e0a9d3ac728ca011e5e6ee2ffc
Eric Horng <erichorng7@gmail.com>
2017-11-10 17:17:56 -0800

    Added home page tests

--- conf.js
+++ conf.js
@@ -1,7 +1,8 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [
-    'registration-spec.js', 
-    'login-spec.js'
+    'registration-spec.js'
+    ,'login-spec.js'
+    ,'home-spec.js'
   ]
 };
